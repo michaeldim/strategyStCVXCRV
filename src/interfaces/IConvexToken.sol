e2a216b5922309595d26106bc1bbab6c1b471a6e
michaeldim <michael.dimmock@virgin.net>
2025-05-10 23:26:31 +0100

    chore: add tool to quickly fix last commit message to conventional format

--- src/interfaces/IConvexToken.sol
+++ src/interfaces/IConvexToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0
-pragma solidity ^0.8.18;
+pragma solidity 0.8.19;
 
 /**
  * @title IConvexToken
@@ -8,10 +8,20 @@ pragma solidity ^0.8.18;
 interface IConvexToken {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
+    function transfer(
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
 
     // Convex specific functions
     function reductionPerCliff() external view returns (uint256);
